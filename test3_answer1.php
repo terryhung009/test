a83717f18c3c609ecbf36d1908c7d00c66302f50
dannyHung <khc741213@gmail.com>
2022-04-30 13:43:22 +0800

    feat:: 改進randomNumber()

--- test3_answer1.php
+++ test3_answer1.php
@@ -1,21 +1,27 @@
 <?php
 require_once('functionPrintList.php');
 
-// $list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,41];
+// $list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42];
+// print_list(range(0, 10));
 function randomNumber()
 {
-    foreach (range(0, 10) as $key => $value) {
+    foreach (range(0, 12) as $key => $value) {
         $key = rand(0, 41);
+
         $list[$key] = true;
-        
-        
-        if(count($list) == 7){
+
+        // echo $key . '<br>';
+        if (count($list) == 7) {
             break;
         }
-
+        
+    }
+    print_list($list);
+    foreach($list as $key=>$value){
+        echo $key . '<br>';
     }
 
-    print_list($list);
+    
 }
 
 randomNumber();
